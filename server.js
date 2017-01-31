427717169130ad13832f5b1adbe7280d3b1de17e
Sisi Li <vivianss0301@gmail.com>
2017-01-30 23:36:35 -0500

    added comments

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+
+// imported 3rd party module 'Connect'
 const connect = require('connect');
 
+//created the connect server object
 const app = connect();
 
+// assigned a port to a constant
+const port = 3000;
 
+//setup the eventloop -listens on port 3000
+app.listen(port);
+console.log('Server running at port: '+ port);
 
-app.listen(3000);
-console.log('Server running at http://localhost:3000/');
+//mounted two routes on the server
 
+// hello route 
 app.use('/hello', (req, res, next) =>{
   res.setHeader('Content-Type', 'text/plain');
   res.end("hello world");
   next();
 });
 
+// main route for the root of my website
 app.use('/', (req, res, next) =>{
   res.setHeader('Content-Type', 'text/plain');
   res.end("Welcome");
